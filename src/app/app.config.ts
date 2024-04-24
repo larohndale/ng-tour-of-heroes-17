67405c8d710b6efa7e2456396a30d62d785ebd17
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-04-24 13:58:03 -0400

    project setup and hero component created

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HeroesComponent } from './heroes/heroes.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes)],
 };
